fcc99f2b507a8e8f0ccb610cad55e5bcf0f50198
Washington Portugal <portugalw@gmail.com>
2023-03-15 11:53:14 -0300

    criando repo e cursos js

---
+++ CURSO/Aula 4/funcoes.js
@@ -0,0 +1,26 @@
+function pulaLinha(){ // Fumção para pular linha
+        document.write("<br><br>")
+    }
+
+    function mostra(frase) {
+
+        document.write("****************************************");
+        pulaLinha();
+        document.write(frase);
+        pulaLinha();
+    }
+    function exibeTitulo(titulo) {
+        document.write("<h1>" + titulo + "</h1>");
+        pulaLinha();
+    }
+
+    function exibeParagrafo(paragrafo) {
+        document.write("<p>" + paragrafo + "</p>");
+        pulaLinha();
+    }
+    
+    function exibirIdade(idadeA, idadeB){
+        diferenca = idadeA - idadeB;
+        mostra("Nossa diferença de idade é: " + diferenca);
+        
+    }
