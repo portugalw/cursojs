c6158cca65a7712812d2443608d09f4597e0ddb8
Washington Portugal <portugalw@gmail.com>
2023-03-29 14:40:11 -0300

    Criando trabalho final e outros

---
+++ FilterJS/TelefonesFab/data.js
@@ -0,0 +1,176 @@
+var data = [
+    {
+        om: 'CCABR',
+        sigla: 'CCABR Anexo B - Térreo',
+        nome: 'Centro de Computação da Aeronáutica de Brasília',
+        telefone: ['Chefia: (61) 2023-1701','Secretaria: (61) 2023-1703','Seção de Atendimento ao Usuário: (61) 2023-1773'],
+        url: 'http://www.portal.intraer/portalintraer/capa/index.php?id=65&page=mostra_catalogo',
+        urlEfetivo: 'https://www.aer.mil.br/organizacoes/mostra/65/CENTRO%20DE%20COMPUTA%C3%87%C3%83O%20DA%20AERON%C3%81UTICA%20DE%20BRAS%C3%8DLIA',
+        urlFoto: 'http://www.portal.intraer/portalintraer/om/logo/mini/ccabr.jpg'
+    },
+    {
+        om: 'COMAER',
+        sigla: 'COMAER - Principal 8º Andar',
+        nome: 'COMANDO DA AERONÁUTICA',
+        telefone: ['Telefone para confirmação: 3966 9624','Secretaria do Gabinete do CMT Aer: 3966 9706'],
+        url: 'http://www.portal.intraer/portalintraer/capa/index.php?id=370&page=mostra_catalogo',
+        urlEfetivo: 'http://www.portal.intraer/portalintraer/capa/index.php?id=370&page=mostra_catalogo',
+        urlFoto: 'http://www.portal.intraer/portalintraer/om/logo/mini/comaer2.jpg'
+    },
+   
+    {
+        om: 'COMGEP',
+        sigla: 'COMGEP - Anexo 2º andar',
+        nome: 'COMANDO-GERAL DO PESSOAL',
+        telefone: ['Protocolo: (61) 2023-1892 <br> Comando: (61) 2023-1854 <br> Comunicação: (61) 2023-2220'],
+        url: 'http://www.portal.intraer/portalintraer/capa/index.php?id=95&page=mostra_catalogo',
+        urlEfetivo: 'http://www.comgep.intraer/index.php/efetivo',
+        urlFoto: 'http://www.portal.intraer/portalintraer/om/logo/mini/comgep2.jpg'
+    },
+    {
+        om: 'CECOMSAER',
+        sigla: 'CECOMSAER -  Principal 7º Andar',
+        nome: 'Centro de Comunicação Social da Aeronáutica',
+        telefone: ['Telefone  61-3966-9636', 'Celular 61-99961-4239'],
+        url: 'http://www.cecomsaer.intraer/index.php/efetivo',
+        urlEfetivo: 'http://www.cecomsaer.intraer/index.php/efetivo',
+        urlFoto: 'http://www.cecomsaer.intraer/images/cecom.png'
+    },
+    {
+        om: 'CENCIAR',
+        sigla: 'CENCIAR - 2º Andar Anexo',
+        nome: 'Centro de Comunicação Social da Aeronáutica',
+        telefone: ['Telefone  61-3966-9636', 'Celular 61-99961-4239'],
+        url: 'https://www.aer.mil.br/organizacoes/mostra/413/CENTRO%20DE%20CONTROLE%20INTERNO%20DA%20AERON%C3%81UTICA',
+        urlEfetivo: 'http://www.cenciar.intraer/index.php/efetivo',
+        urlFoto: 'http://www.gapbr.intraer/images/GAPBR/UNIDADES-2018/CENCIAR.png'
+    },
+    {
+        om: 'CIAER',
+        sigla: 'CIAER - Anexo 4º Andar',
+        nome: 'Centro de Inteligência da Aeronáutica',
+        telefone: ['Tel: (61) 3041-9300', ''],
+        url: 'http://www.ciaer.intraer/index.php',
+        urlEfetivo: 'http://www.ciaer.intraer/index.php',
+        urlFoto: 'http://www.ciaer.intraer/images/banners/ciaer_medio.png'
+    },
+    {
+        om: 'GAPBR',
+        sigla: 'GAPBR - Anexo Térreo ',
+        nome: 'GRUPAMENTO DE APOIO DE BRASÍLIA ',
+        telefone: ['Seção de Registro: 2023 2660 / 2023 2771', 'Secretaria: 2023 2707 / 2023 2636 / 2023 2610'],
+        url: 'https://www.aer.mil.br/organizacoes/mostra/371/GRUPAMENTO%20DE%20APOIO%20DE%20BRAS%C3%8DLIA',
+        urlEfetivo: 'http://www.gapbr.intraer/index.php/2015-11-09-14-07-30/catalago-telefonico/ramais-gapbr',
+        urlFoto: 'http://www.gapbr.intraer/images/banners/Novo_Logo_GAPBR.png'
+    },
+    {
+        om: 'CPO',
+        sigla: 'CPO -  Anexo A - 3º Andar',
+        nome: 'SECPROM -  Secretaria de Avaliação e Promoções',
+        telefone: ['Portaria da SECPROM: 2023-2100'],
+        url: 'http://www.secprom.intraer/index.php/fale-com-a-cpo',
+        urlEfetivo: 'http://www.secprom.intraer/index.php/fale-com-a-cpo',
+        urlFoto: 'http://www.gapbr.intraer/images/GAPBR/UNIDADES-2018/CPO.png'
+    },
+    {
+        om: 'COPAC',
+        sigla: 'COPAC -  Anexo 3º Andar',
+        nome: 'COMISSÃO COORDENADORA DO PROGRAMA AERONAVE DE COMBATE',
+        telefone: ['Portaria: 2023 1977 / 2023 1978'],
+        url: 'https://www.fab.mil.br/organizacoes/mostra/101/',
+        urlEfetivo: 'https://www.fab.mil.br/organizacoes/mostra/101/',
+        urlFoto: 'https://www.fab.mil.br/om/logo/mini/copac.jpg'
+    },
+    {
+        om: 'DIRENS',
+        sigla: 'DIRENS - Anexo - 1º Andar',
+        nome: 'Diretoria de Ensino',
+        telefone: ['PORTARIA: (61) 2023-2247 <br> PROTOCOLO (0xx61) 2023-2243'],
+        url: 'https://www.aer.mil.br/organizacoes/mostra/118/DIRETORIA%20DE%20ENSINO',
+        urlEfetivo: 'http://www.direns.intraer/index.php?option=com_content&view=article&id=199&Itemid=467',
+        urlFoto: 'http://www.gapbr.intraer/images/GAPBR/UNIDADES-2018/DIRENS.png'
+    },
+    {
+        om: 'EMAER',
+        sigla: 'EMAER - Principal 5º Andar',
+        nome: 'ESTADO-MAIOR DA AERONÁUTICA',
+        telefone: ['Telefone: (61) 3961-8437 '],
+        url: 'http://www.portal.intraer/portalintraer/capa/index.php?id=%20210&page=mostra_catalogo',
+        urlEfetivo: 'http://www.emaer.intraer/portal/index.php/form-contato',
+        urlFoto: 'http://www.portal.intraer/portalintraer/om/logo/mini/emaer.jpg'
+    },
+    {
+        om: 'GABAER',
+        sigla: 'GABAER - Principal  7º Andar',
+        nome: 'GABINETE DO COMANDANTE DA AERONÁUTICA',
+        telefone: ['Protocolo e Arquivo: 3966-9167 ', 'Assessoria de Recursos Humanos (GC1): 3966-9184'],
+        url: 'https://www.aer.mil.br/organizacoes/mostra/569/GABINETE%20DO%20COMANDANTE%20DA%20AERON%C3%81UTICA',
+        urlEfetivo: 'http://www.gabaer.intraer/agenda/index.php',
+        urlFoto: 'http://www.portal.intraer/portalintraer/om/logo/mini/gabaer.jpg'
+    },
+    {
+        om: 'ASPAER',
+        sigla: 'ASPAER - Principal Térreo',
+        nome: 'Assessoria Parlamentar e de Relações Institucionais do Comando da Aeronáutica',
+        telefone: ['Divisão Administrativa (DA) :  Chefe: 3966-9176 <br>  Secretaria: 3966-9689 <br>   Protocolo: 3966-9682'],
+        url: 'https://www.aer.mil.br/organizacoes/mostra/32/ASSESSORIA%20PARLAMENTAR%20E%20DE%20RELA%C3%87%C3%95ES%20INSTITUCIONAIS%20DO%20COMANDANTE%20DA%20AERON%C3%81UTICA',
+        urlEfetivo: 'http://www.aspaer.intraer/agenda/index.php',
+        urlFoto: 'https://www.fab.mil.br/om/logo/mini/aspaer.jpg'
+    },
+    {
+        om: 'OABR',
+        sigla: 'OABR - Sobreloja',
+        nome: 'ODONTOCLÍNICA DE AERONÁUTICA DE BRASÍLIA',
+        telefone: ['Recepção / Marcação de Consultas 2023 2882 / 2023 2885'],
+        url: 'https://www.aer.mil.br/organizacoes/mostra/245/ODONTOCL%C3%8DNICA%20DE%20AERON%C3%81UTICA%20DE%20BRAS%C3%8DLIA',
+        urlEfetivo: 'http://www.oabr.intraer/index.php?option=com_efetivowxfab&view=efetivo&Itemid=503',
+        urlFoto: 'http://www.oabr.intraer/images/banners/OABR_DOM1.png'
+    },
+    {
+        om: 'SEFA',
+        sigla: 'SEFA - Principal  3º Andar',
+        nome: 'SECRETARIA DE ECONOMIA, FINANÇAS E ADMINISTRAÇÃO DA AERONÁUTICA - 3º Andar',
+        telefone: ['Recepção / Marcação de Consultas 2023 2882 / 2023 2885'],
+        url: 'https://www.aer.mil.br/organizacoes/mostra/245/ODONTOCL%C3%8DNICA%20DE%20AERON%C3%81UTICA%20DE%20BRAS%C3%8DLIA',
+        urlEfetivo: 'http://www.oabr.intraer/index.php?option=com_efetivowxfab&view=efetivo&Itemid=503',
+        urlFoto: 'https://www.fab.mil.br/om/logo/mini/sefa.jpg'
+    },
+    {
+        om: 'COJAER ',
+        sigla: 'COJAER - Principal - 7º Andar',
+        nome: 'Consultoria Jurídica-Adjunta do Comando da Aeronáutica - 7º Andar',
+        telefone: ['Secretária Consultor: 3966 9626 ',' Protocolo: 3966-9607 / 3966-9627 / 3966-9628'],
+        url: 'http://www.cojaer.intraer/agenda/',
+        urlEfetivo: 'http://www.cojaer.intraer/agenda/',
+        urlFoto: 'http://www.cojaer.intraer/images/banners/cojaer.png'
+    },
+    {
+        om: 'COMAE',
+        sigla: 'COMAE',
+        nome: 'COMANDO DE OPERAÇÕES AEROESPACIAIS',
+        telefone: ['Portaria: 3364 7900 / 3364 7999','Protocolo (SPDA): 3364 7969 / 3364 7967 / 3364 7932'],
+        url: 'http://www.portal.intraer/portalintraer/capa/index.php?id=455&page=mostra_catalogo',
+        urlEfetivo: 'http://www.cecomsaer.intraer/index.php/efetivo',
+        urlFoto: 'http://www.portal.intraer/portalintraer/om/logo/mini/comae.png'
+    },
+    {
+        om: 'COMPREP',
+        sigla: 'COMPREP',
+        nome: 'COMANDO DE PREPARO',
+        telefone: ['Portaria: 3364 8900','Secretaria do Comando: 3364 8905'],
+        url: 'http://www.portal.intraer/portalintraer/capa/index.php?id=95&page=mostra_catalogo',
+        urlEfetivo: 'http://www.cecomsaer.intraer/index.php/efetivo',
+        urlFoto: 'http://www.portal.intraer/portalintraer/om/logo/mini/comprep.jpg'
+    },
+    
+]
+
+var columns = {
+    om: 'OM',
+    sigla: 'Sigla',
+    nome: 'Nome',
+    url: 'Telefones',
+    telefone: 'Telefones',
+    
+    
+}
